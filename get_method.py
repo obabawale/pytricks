bcba84b528f5d1d479337a10caa5e3e9637bd901
obabawale <oebabawale@rocketmail.com>
2018-10-11 11:56:27 +0100

    Add get_method script

---
+++ get_method.py
@@ -0,0 +1,16 @@
+# The get() method on dicts
+# and its "default" argument
+
+name_for_userid = {
+    382: "Alice",
+    590: "Bob",
+    951: "Dilbert",
+}
+
+def greeting(userid):
+    return "Hi %s!" % name_for_userid.get(userid, "there")
+
+if __name__ == "__main__":
+    print(greeting(382))
+
+    print(greeting(333333))
